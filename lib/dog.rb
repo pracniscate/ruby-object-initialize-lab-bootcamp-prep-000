d357b5780297e44b382f9a3a2610287cd6874632
Lorena <lorena.giddens@outlook.com>
2018-05-10 19:03:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,7 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
   end
   
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
+
 end
